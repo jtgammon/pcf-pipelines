fe3649d4c8ee1d8ef04ebd89000275dab8966110
Kris Hicks <khicks@pivotal.io>
2017-06-01 14:48:18 -0700

    fixup! Make aws-pcf-prepare idempotent

--- tasks/install-pcf-aws/prepare-aws/task.sh
+++ tasks/install-pcf-aws/prepare-aws/task.sh
@@ -6,13 +6,13 @@ root=$(pwd)
 ami=$(cat ami/ami)
 
 terraform plan \
-  pcf-pipelines/tasks/install-pcf-aws/terraform \
   -state terraform-state/terraform.tfstate \
   -var "opsman_ami=${ami}" \
   -var "db_master_username=${DB_MASTER_USERNAME}" \
   -var "db_master_password=${DB_MASTER_PASSWORD}" \
   -var "prefix=${TERRAFORM_PREFIX}" \
-  -out terraform.tfplan
+  -out terraform.tfplan \
+  pcf-pipelines/tasks/install-pcf-aws/terraform
 
 terraform apply \
   -state-out $root/terraform-state/terraform.tfstate \
