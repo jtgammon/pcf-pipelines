873122298a9365543bf737f57c4647a7b8d5a072
Kris Hicks <khicks@pivotal.io>
2017-06-01 12:58:08 -0700

    Use individual service db creds in install-pcf/aws

--- tasks/install-pcf-aws/prepare-aws/task.sh
+++ tasks/install-pcf-aws/prepare-aws/task.sh
@@ -9,6 +9,8 @@ terraform plan \
   pcf-pipelines/tasks/install-pcf-aws/terraform \
   -state terraform-state/terraform.tfstate \
   -var "opsman_ami=${ami}" \
+  -var "db_master_username=${DB_MASTER_USERNAME}" \
+  -var "db_master_password=${DB_MASTER_PASSWORD}" \
   -out terraform.tfplan
 
 terraform apply \
